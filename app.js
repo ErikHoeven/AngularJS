53558ede0b37e9ec9e09e3acb670ac0a07a1928c
Erik van der Hoeven <Erik.Hoeven@gmail.com>
2015-08-03 15:20:57 +0200

    Initieel 0.2

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+    var currentstep = 0;
+    
+    $("#step1").hide();
+    $("#step2").hide();
+    
+    
+    $("#btnStep1").click(function() {
+        
+        $("#step1").show();
+        
+     
+        
+    }
+    
+    
+});
